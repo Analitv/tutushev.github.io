d2ec665822cff55cb6389d5d0f34a0ac2b18487f
Nikita Tutushev <n.tutushev@gmail.com>
2020-05-28 14:34:46 +0500

    Updates

---
+++ precache-manifest.533740d22caee8db4c0b24428976c81d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70bab30b6dc404e66b9616126135620a",
+    "url": "/keepcopy/index.html"
+  },
+  {
+    "revision": "ee74e9206a5e40a1aa18",
+    "url": "/keepcopy/static/css/main.cad69472.chunk.css"
+  },
+  {
+    "revision": "34a596926e4271260866",
+    "url": "/keepcopy/static/js/2.8d061ace.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/keepcopy/static/js/2.8d061ace.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee74e9206a5e40a1aa18",
+    "url": "/keepcopy/static/js/main.1f37dc1b.chunk.js"
+  },
+  {
+    "revision": "0dbd5aa55f8748c6e113",
+    "url": "/keepcopy/static/js/runtime-main.3f09913b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/keepcopy/static/media/logo.5d5d9eef.svg"
+  }
+]);
